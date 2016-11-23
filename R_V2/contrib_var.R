fac3bce3bf18e33214eff9589185d5a5f06f9047
BeaGir <beatrice.girard@berkeley.edu>
2016-11-23 11:07:21 -0800

    Update contrib_var.R

--- R_V2/contrib_var.R
+++ R_V2/contrib_var.R
@@ -1,7 +1,9 @@
 #' @title contrib_var
 #' @description Computes the contribution of each variable to each dimension \code{"contrib_var"}
-#' @param mfaex, an object of class "mfa"
-#' @return the matrix of contributions M[j,l] is the contribution of variable k to dimension l
+#' @param mfaex, an object of class "mfa" builded from an original data-set containing the 'tables'
+#' @param tables, some original tables
+#' @param setex, the sets of variables used in the MFA (mapping between the tables and the mfa variables)
+#' @return the matrix of contributions for all variables : M[j,l] is the contribution of variable k to dimension l
 #' @export
 #' @examples
 #' 
@@ -11,18 +13,20 @@
 #' tables <- data_tables(wines, sets, TRUE, scaling_vec)
 #' mymfa1 <- mfa(wines, sets, ncomps = 2, center = TRUE, scale = scaling_vec)
 #' 
+#' sets <- lapply(sets, FUN=function(vec){vec-1})
 #' contrib_var1 <- contrib_var(mymfa1, sets, tables)
 
 
-contrib_var <- function(mfaex, setex, tables){
+contrib_var <- function(mfaex, sets, tables){
   # INPUT
-  # An mfa object
+  # An mfa object, builded from the given tables
   # The sets of indices defining the tables
   # The pre-processed tables
-  
   # OUTPUT
   # A matrix containing the contribution of each variables to the components
   # ctr[j,l] = contrib. var j to the component l
+  
+  check_contrib(mfaex, sets, tables)
   
   # We define Q, F and the partial F matrices.
   Q = mfaex$loadings
@@ -34,7 +38,6 @@ contrib_var <- function(mfaex, setex, tables){
   l = ncol(Q)
   
   # Tables
-  sets = lapply(setex, FUN=function(vec){vec-1})
   K = length(sets)
   
   
@@ -56,4 +59,34 @@ contrib_var <- function(mfaex, setex, tables){
   # We compute the contributions and store it in a matrix.
   ctr = (Q^2)*alpha
   ctr
+}
+
+check_contrib <- function(mfaex, sets, tables){
+  if(!(class(mfaex) == "mfa")){
+    stop("\n 'mfaex must be an object of class mfa")
+  }
+  if (!(is.list(sets))){
+    stop("\n 'sets' must be a list")
+  }
+  if (!(is.list(tables))){
+    stop("\n 'tables' must be a list")
+  }
+  if (length(sets) != length(tables)){
+    stop("\n 'sets' and 'tables' must have the same number of elements")
+  }
+  for (i in 1:length(sets)){
+    if (!is.numeric(sets[[i]])){
+      stop("\n 'sets' must be a list of numeric indices")
+    }
+    if (!(is.numeric(tables[[i]]))){
+      stop("\n 'tables' must contain only numeric elements")
+    }
+    if (min(sets[[i]] <=0)|max(sets[[i]])> nrow(mfaex$loadings)){
+      stop("\n 'sets' must be consistent with the number of variables in the mfa")
+    }
+    if (length(sets[[i]]) != ncol(tables[[i]])){
+      stop("\n 'sets' must be a consistent mapping of the tables columns to the mfa variables.")
+    }
+  }
+  TRUE
 }
