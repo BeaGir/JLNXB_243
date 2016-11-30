926ea735474aae89b3ea08416af24d10095f7918
Alina <xiaoqi_zhang@berkeley.edu>
2016-11-29 18:36:10 -0800

    Update plot.mfa.R

--- plot.mfa.R
+++ plot.mfa.R
@@ -22,30 +22,30 @@
 #' plot(mymfa, type = 5, X = 1)
 #'
 #' @export
-plot.mfa <- function(mfa, type, d1 = 1, d2 = 2, X = 1, loading_labels = NULL, ...) {
+plot.mfa <- function(mfa, type, d1 = 1, d2 = 2, X = 1, loading_labels = NULL, cex = 1, ...) {
   if (type != 1 & type != 2 & type != 3 & type != 4 & type != 5) {
     stop("invalid type input: pecify from 1, 2, 3, 4 or 5")
   }
   else {
     #type = 1, plot_factor_scores
     if (type == 1) {
-      plot_factor_scores(mymfa = mfa, d1 = d1, d2 = d2)
+      plot_factor_scores(mymfa = mfa, d1 = d1, d2 = d2, cex = cex)
     }
     #type = 2, plot_pfs_vl
     else if (type ==2 ) {
-      plot_pfs_vl(mymfa = mfa, X=X, d1 = d1, d2 = d2, loading_labels = loading_labels)
+      plot_pfs_vl(mymfa = mfa, X=X, d1 = d1, d2 = d2, loading_labels = loading_labels,  cex = cex)
     }
     #type = 4, plot_pfs
     else if (type ==4 ) {
-      plot_pfs(mymfa = mfa, X=X, d1 = d1, d2 = d2)
+      plot_pfs(mymfa = mfa, X=X, d1 = d1, d2 = d2,  cex = cex)
     }
     #type = 5, plot_vl
     else if (type ==5 ) {
-      plot_vl(mymfa = mfa, X=X, d1 = d1, d2 = d2, loading_labels = loading_labels)
+      plot_vl(mymfa = mfa, X=X, d1 = d1, d2 = d2, loading_labels = loading_labels , cex = cex)
     }
     #type = 3, plot_pfs_vl_all
     else {
-      plot_pfs_vl_all(mymfa=mfa, d1 = d1, d2 = d2, loading_labels = loading_labels)
+      plot_pfs_vl_all(mymfa=mfa, d1 = d1, d2 = d2, loading_labels = loading_labels, cex = cex)
     }
   }
 }
@@ -55,7 +55,7 @@ plot.mfa <- function(mfa, type, d1 = 1, d2 = 2, X = 1, loading_labels = NULL, ..
 #mymfa: the mfa object
 #d1: horizontal axes
 #d2: vertical axes
-plot_factor_scores <- function(mymfa, d1 = 1, d2 = 2) {
+plot_factor_scores <- function(mymfa, d1 = 1, d2 = 2,  cex = 1) {
   if (ncol(mymfa$common_factor_scores) < max(d1, d2)) {
     stop("invalid dimension input: common factor score does not have enough dimension")
   }
@@ -68,27 +68,27 @@ plot_factor_scores <- function(mymfa, d1 = 1, d2 = 2) {
          xlim = c(-1*margin-0.5,margin+0.5),
          ylim = c(-1*margin-0.5,margin+0.5),
          xlab = NA,
-         ylab = NA, main = "Factor Scores", cex = 2, cex.main = 3)
-    legend("bottomleft", cex=1.8 ,legend = data$objects ,col=data$cl ,pch=16)
+         ylab = NA, main = "Factor Scores", cex = cex, cex.main = cex*1.5)
+    legend("bottomleft", cex=0.7 *cex ,legend = data$objects ,col=data$cl ,pch=16)
     arrows(x0 = -1*margin-0.2, y0 = 0, x1 = margin+0.2, y1 = 0, length=0.05,angle=20,
            code = 2, lwd = 2)
     arrows(y0 = -1*margin-0.2, x0 = 0, y1 = margin+0.2, x1 = 0, length=0.05,angle=20,
            code = 2, lwd = 2)
     d1_string <- paste(d1)
     d2_string <- paste(d2)
-    text(0, margin+0.3, d2_string, cex= 2)
-    text(margin+0.3, 0, d1_string, cex= 2)
+    text(0, margin+0.3, d2_string, cex= cex)
+    text(margin+0.3, 0, d1_string, cex= cex)
   }
 }
 
 #plot for partial factor scores
-plot_pfs <- function (mymfa, X=1, d1 = 1, d2 = 2) {
+plot_pfs <- function (mymfa, X=1, d1 = 1, d2 = 2, cex = 1) {
   if (ncol(mymfa$common_factor_scores) < max(d1, d2)) {
     stop("invalid dimension input: partial factor score does not have enough dimension")
   }
   else {
     data <- data.frame(mymfa$partial_factor_scores[[X]],objects = mymfa$observation_names, cl = rainbow(nrow(mymfa$common_factor_scores)))
-
+    
     margin <- max(max(abs(data[,d1])),max(abs(data[,d2])))
     plot(data[, d1], data[, d2], col= data$cl, pch=16, axes = FALSE,
          panel.first = grid(),
@@ -96,29 +96,29 @@ plot_pfs <- function (mymfa, X=1, d1 = 1, d2 = 2) {
          ylim = c(-1*margin-0.5,margin+0.5),
          xlab = NA,
          ylab = NA,
-         main ="Partial Factor Scores" , cex = 2, cex.main = 3)
-    legend("bottomleft", cex= 1.8 ,legend = data$objects ,col=data$cl ,pch=16)
+         main ="Partial Factor Scores" , cex = cex, cex.main = 1.5 * cex)
+    legend("bottomleft", cex= 0.7 * cex ,legend = data$objects ,col=data$cl ,pch=16)
     arrows(x0 = -1*margin-0.2, y0 = 0, x1 = margin+0.2, y1 = 0, length=0.05,angle=20,
            code = 2, lwd = 2)
     arrows(y0 = -1*margin-0.2, x0 = 0, y1 = margin+0.2, x1 = 0, length=0.05,angle=20,
            code = 2, lwd = 2)
     d1_string <- paste(d1)
     d2_string <- paste(d2)
-    text(0, margin+0.3, d2_string, cex= 2)
-    text(margin+0.3, 0, d1_string, cex= 2)
-
+    text(0, margin+0.3, d2_string, cex= cex)
+    text(margin+0.3, 0, d1_string, cex= cex)
+    
   }
 }
 
 
 
 #plot for variable loadings
-plot_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL) {
+plot_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL, cex = 1) {
   if (ncol(mymfa$common_factor_scores) < max(d1, d2)) {
     stop("invalid dimension input: variable loading do not have enough dimension")
   }
   else {
-
+    
     loadingdata <- data.frame(mymfa$partial_loadings[[X]] )
     #talked to Gaston, he said the rescale in the paper has some issues
     #Gaston confirmed that we can rescale using the factors as we want
@@ -132,7 +132,7 @@ plot_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL) {
       v <- loading_labels
     }
     data <- data.frame(rescaled_loadings, objects = v, cl = rainbow(length(v)))
-
+    
     margin <- max(max(abs(data[,1])),max(abs(data[,2])))
     plot(data[, 1], data[, 2], col= data$cl, pch=16, axes = FALSE,
          panel.first = grid(),
@@ -140,13 +140,13 @@ plot_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL) {
          ylim = c(-1*margin-0.5,margin+0.5),
          xlab = NA,
          ylab = NA,
-         main = "Variable Loadings" , cex = 2, cex.main = 3)
+         main = "Variable Loadings" , cex = cex, cex.main = 1.5 * cex)
     if (!is.null(loading_labels)) {
       #decided to let user input data labels themselves since it is not included in csv file
-      text(rescaled_loadings[,d1], rescaled_loadings[,d2], labels=loading_labels[1:nrow(rescaled_loadings)], cex= 2, pos=4)
+      text(rescaled_loadings[,d1], rescaled_loadings[,d2], labels=loading_labels[1:nrow(rescaled_loadings)], cex= 0.7*cex, pos=4)
     }
     else {
-      legend("bottomleft", cex= 1.8,legend = data$objects ,col=data$cl ,pch=16)
+      legend("bottomleft", cex= 0.7 * cex,legend = data$objects ,col=data$cl ,pch=16)
     }
     arrows(x0 = -1*margin-0.2, y0 = 0, x1 = margin+0.2, y1 = 0, length=0.05,angle=20,
            code = 2, lwd = 2)
@@ -154,9 +154,9 @@ plot_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL) {
            code = 2, lwd = 2)
     d1_string <- paste(d1)
     d2_string <- paste(d2)
-    text(0, margin+0.3, d2_string, cex= 2)
-    text(margin+0.3, 0, d1_string, cex= 2)
-
+    text(0, margin+0.3, d2_string, cex= cex)
+    text(margin+0.3, 0, d1_string, cex= cex)
+    
   }
 }
 
@@ -166,18 +166,18 @@ plot_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL) {
 #d2: vertical axes
 #X: which data table you would like to plot for
 #loading_labels: the label that user could input themselves
-plot_pfs_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL) {
+plot_pfs_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL, cex = 1) {
   if (ncol(mymfa$common_factor_scores) < max(d1, d2)) {
     stop("invalid dimension input: factor score  and loadings do not have enough dimension")
   }
   else {
     data <- data.frame(mymfa$partial_factor_scores[[X]],objects = mymfa$observation_names, cl = rainbow(nrow(mymfa$common_factor_scores)))
-
+    
     loadingdata <- data.frame(mymfa$partial_loadings[[X]] )
     #talked to Gaston, he said the rescale in the paper has some issues
     #Gaston confirmed that we can rescale using the factors as we want
     rescaled_loadings <- data.frame(x = 0.8*loadingdata[,d1]/sd(loadingdata[,d1]), y = 0.4*loadingdata[,d2]/sd(loadingdata[,d2]))
-
+    
     margin <- max(max(abs(data[,d1])),max(abs(data[,d2])), max(abs(rescaled_loadings[,d1])), max(abs(rescaled_loadings[,d2])))
     plot(data[, d1], data[, d2], col= data$cl, pch=16, axes = FALSE,
          panel.first = grid(),
@@ -185,21 +185,21 @@ plot_pfs_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL) {
          ylim = c(-1*margin-0.5,margin+0.5),
          xlab = NA,
          ylab = NA,
-         main = "Partial Factor Score and Variable Loadings" , cex = 2, cex.main = 2)
-    legend("bottomleft", cex= 1.8 ,legend = data$objects ,col=data$cl ,pch=16)
+         main = "Partial Factor Score \nand Variable Loadings" , cex = cex, cex.main = 1 * cex)
+    legend("bottomleft", cex= 0.7 * cex ,legend = data$objects ,col=data$cl ,pch=16)
     arrows(x0 = -1*margin-0.2, y0 = 0, x1 = margin+0.2, y1 = 0, length=0.05,angle=20,
            code = 2, lwd = 2)
     arrows(y0 = -1*margin-0.2, x0 = 0, y1 = margin+0.2, x1 = 0, length=0.05,angle=20,
            code = 2, lwd = 2)
     d1_string <- paste(d1)
     d2_string <- paste(d2)
-    text(0, margin+0.3, d2_string, cex= 2)
-    text(margin+0.3, 0, d1_string, cex= 2)
-
-    points(rescaled_loadings[,d1], rescaled_loadings[,d2], col = "grey", pch=12, cex = 2)
+    text(0, margin+0.3, d2_string, cex= cex)
+    text(margin+0.3, 0, d1_string, cex= cex)
+    
+    points(rescaled_loadings[,d1], rescaled_loadings[,d2], col = "grey", pch=12, cex = cex)
     if (!is.null(loading_labels)) {
       #decided to let user input data labels themselves since it is not included in csv file
-      text(rescaled_loadings[,d1], rescaled_loadings[,d2], labels=loading_labels[1:nrow(rescaled_loadings)], cex= 2, pos=4)
+      text(rescaled_loadings[,d1], rescaled_loadings[,d2], labels=loading_labels[1:nrow(rescaled_loadings)], cex= 0.7*cex, pos=4)
     }
   }
 }
@@ -209,7 +209,7 @@ plot_pfs_vl <- function (mymfa, X=1, d1 = 1, d2 = 2, loading_labels = NULL) {
 #d1: horizontal axes
 #d2: vertical axes
 #loading_labels: the label that user could input themselves
-plot_pfs_vl_all <- function (mymfa, d1 = 1, d2 = 2, loading_labels = NULL) {
+plot_pfs_vl_all <- function (mymfa, d1 = 1, d2 = 2, loading_labels = NULL, cex = 1) {
   total <- length(mymfa$partial_factor_scores)
   if (is.null(loading_labels)) {
     #the reason I didn't output all plots into one page
@@ -221,13 +221,13 @@ plot_pfs_vl_all <- function (mymfa, d1 = 1, d2 = 2, loading_labels = NULL) {
     #thus I am plotting them out one by one in new windows
     for (i in 1:total) {
       #dev.new()
-      plot_pfs_vl(mymfa, i, d1, d2, loading_labels)
+      plot_pfs_vl(mymfa, i, d1, d2, loading_labels, cex = cex)
     }
   }
   else {
     for (i in 1:total) {
       #dev.new()
-      plot_pfs_vl(mymfa, i, d1, d2, loading_labels = loading_labels[,i])
+      plot_pfs_vl(mymfa, i, d1, d2, loading_labels = loading_labels[,i], cex = cex)
     }
   }
 }
